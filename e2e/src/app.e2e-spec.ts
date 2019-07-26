6dc34807546c2127b7228c5d09d584e9420e5398
DFSE <dfse@DFSEs-MacBook-Pro.local>
2019-07-26 14:23:09 +0200

    Make the test work on appium

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { Ng2RealApp } from './app.po';
+import { browser } from 'protractor';
 
 describe('ng-demo App', () => {
   let page: Ng2RealApp;
@@ -8,7 +9,9 @@ describe('ng-demo App', () => {
   });
 
   it('should display message saying app works', async () => {
+    await browser.waitForAngularEnabled(false);
     await page.navigateTo();
     expect(await page.getParagraphText()).toContain('conduit');
+    await browser.waitForAngularEnabled(true);
   });
 });
